7afbc8a8c36a2b02e22f968e0e68b39ace4e2689
Dmitriy Bormotov <squier7@gmail.com>
2020-05-05 20:49:26 +0500

    update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     hostname: 'localhost',
     port: 4444,
-    path: '/',
+    path: '/wd/hub',
     //
     // ====================
     // Runner Configuration
@@ -108,7 +108,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    // services: ['chromedriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
